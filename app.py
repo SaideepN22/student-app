b21c7a304f3e3621dfbb615122b1102d54523c85
Saideep Nayak <saideepnayak123@gmail.com>
2026-04-21 00:18:00 +0530

    fixed for deployment

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def edit(index):
     return render_template('edit.html', student=students[index], index=index)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
